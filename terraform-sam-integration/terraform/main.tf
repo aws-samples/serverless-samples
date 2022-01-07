46d2178779443a4fb3a5368b218eb52ab4dc7feb
Daniel Fox <dfxamzn@amazon.com>
2022-01-07 09:49:21 -0700

    add encryption

--- terraform-sam-integration/terraform/main.tf
+++ terraform-sam-integration/terraform/main.tf
@@ -10,6 +10,7 @@ resource "aws_sqs_queue" "terraform_queue" {
   max_message_size          = 2048
   message_retention_seconds = 86400
   receive_wait_time_seconds = 10
+  sqs_managed_sse_enabled = true
 
   tags = {
     Environment = "example app"
