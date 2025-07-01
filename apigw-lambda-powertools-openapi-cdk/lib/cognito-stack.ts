1039e3d8e3c33241e39757b5e1a7fe74636a8256
Robert Hanuschke <robhan@amazon.com>
2025-07-01 13:27:09 +0200

    cdk_nag findings on Cognito

--- apigw-lambda-powertools-openapi-cdk/lib/cognito-stack.ts
+++ apigw-lambda-powertools-openapi-cdk/lib/cognito-stack.ts
@@ -1,6 +1,7 @@
 // lib/cognito-stack.ts
 import * as cdk from "aws-cdk-lib";
 import * as cognito from "aws-cdk-lib/aws-cognito";
+import { NagSuppressions } from "cdk-nag";
 import { Construct } from "constructs";
 
 export interface CognitoStackProps extends cdk.NestedStackProps {
@@ -15,10 +16,19 @@ export class CognitoStack extends cdk.NestedStack {
     super(scope, id, props);
 
     this.userPool = new cognito.UserPool(this, "UserPool", {
+      featurePlan: cognito.FeaturePlan.PLUS,
       removalPolicy:
         props.stageName === "dev"
           ? cdk.RemovalPolicy.DESTROY
           : cdk.RemovalPolicy.RETAIN,
+      passwordPolicy: {
+        minLength: 8,
+        requireUppercase: true,
+        requireDigits: true,
+        requireSymbols: true,
+      },
+      standardThreatProtectionMode:
+        cognito.StandardThreatProtectionMode.FULL_FUNCTION,
     });
 
     this.userPoolClient = new cognito.UserPoolClient(this, "UserPoolClient", {
@@ -28,5 +38,12 @@ export class CognitoStack extends cdk.NestedStack {
         adminUserPassword: true,
       },
     });
+
+    NagSuppressions.addResourceSuppressions(this.userPool, [
+      {
+        id: "AwsSolutions-COG2",
+        reason: "Sample does not need MFA, would overcomplicate demo flow",
+      },
+    ]);
   }
 }
